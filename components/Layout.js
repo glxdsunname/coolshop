a2e621a4c0dfd19e56595bc51d1d42907b63cc4c
glxdsunname <lamkhmt07@gmail.com>
2021-01-06 23:02:45 +0700

    Layout

---
+++ components/Layout.js
@@ -0,0 +1,82 @@
+import { ThemeProvider } from '@material-ui/core'
+import React, { useContext, useEffect } from 'react';
+import { 
+    AppBar, 
+    CssBaseline, 
+    Toolbar,
+    Link,
+    CircularProgress,
+    Badge,
+    Box,
+    Typography,
+    Container
+ } from "@material-ui/core";
+import Head from 'next/head';
+import { theme, useStyles } from '../utils/styles.js';
+import NextLink from 'next/link';
+
+export const Layout = ({children, commercePublicKey, title  = 'Coolshop'}) => {
+
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <Head>
+                <meta charSet="utf-8"/>
+                <title>{`${title} - Coolshop`}</title>
+                <link rel="icon" href="favicon.icon" />
+                <meta name = "viewport" content="width=device-width, inital-scale=1, shrink-to-fit=no"/>
+            </Head>
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          className={classes.appBar}
+        >
+          <Toolbar className={classes.toolbar}>
+            <NextLink href="/">
+              <Link
+                variant="h6"
+                color="inherit"
+                noWrap
+                href="/"
+                className={classes.toolbarTitle}
+              >
+                Coolshop
+              </Link>
+            </NextLink>
+
+            <nav>
+              <NextLink href="/cart">
+                <Link
+                  variant="button"
+                  color="textPrimary"
+                  href="/cart"
+                  className={classes.link}
+                >
+                  Cart
+                </Link>
+              </NextLink>
+            </nav>
+          </Toolbar>
+        </AppBar>
+        {/* Hero unit */}
+        <Container component="main" className={classes.main}>
+          {children}
+        </Container>
+        {/* End hero unit */}
+        <Container maxWidth="md" component="footer">
+          <Box mt={5}>
+            <Typography variant="body2" color="textSecondary" align="center">
+              {'© '}
+              {`Coolshop`} 2021
+              {'.'}
+            </Typography>
+          </Box>
+        </Container>
+        {/* End footer */}
+      </ThemeProvider>
+        </React.Fragment>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import '../styles/globals.css'
+import React, { useContext, useEffect } from 'react';
+import {StoreProvider} from '../components/Store'; 
+import NProgeress from 'nprogress';
+import Router from 'next/router';
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.start());
+Router.events.on('routeChangeError', () => nProgress.start());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, [])
+
+  return(
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  )
 }
 
 export default MyApp
+
+MyApp.getInititalProps = async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  };
+}
